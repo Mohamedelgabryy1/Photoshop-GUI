f709aa8780af289d0efd456052db28549ec34664
Mohamed ElGabry <162322809+Mohamedelgabryy1@users.noreply.github.com>
2024-04-17 17:38:16 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+/*
+ * FCAI Structured Programming  2024 - Assignment 3
+ *
+ * CS112_A3_Part3_18_2023620.cpp
+ *
+ * Program Description: The Mini Photoshop program is designed to perform various image processing
+ * tasks on images loaded into the application of the images according to their preferences.
+ *
+ * Last Modification Date: 17/4/2024
+ *
+ * Mohamed Mahmoud Bayoumi Mohamed ElGabry , 20230620 , Section 18 , mohammed.elgabry7@gmail.com
+ *
+ * Teaching Assistant: Ahmad Lotfy
+ *
+ * Github link: https://github.com/Mohamedelgabryy1/Photoshop-GUI
+ *
+*/
 #include "mainwindow.h"
 
 #include <QApplication>
